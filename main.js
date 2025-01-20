222c44ed7f3f1ae8b16aac99f165f969e7922d1f
Gökce Gökay Tutcu <166149068+GGT-Design@users.noreply.github.com>
2025-01-20 22:09:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -58,6 +58,8 @@ document.addEventListener("DOMContentLoaded", () => {
   messschieber.addEventListener('touchstart', startDrag);
 
   function startDrag(e) {
+    e.preventDefault();
+    
     if (isDragging) return;
     isDragging = true;
     messschieber.style.transition = 'none';
